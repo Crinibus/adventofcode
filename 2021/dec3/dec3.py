68ee4e433a32c086a9a1af94c16305ea600e108c
Crinibus <57172157+Crinibus@users.noreply.github.com>
2021-12-19 17:36:31 +0100

    2021 dec 3 part 1 completed

--- 2021/dec3/dec3.py
+++ 2021/dec3/dec3.py
@@ -1,4 +1,5 @@
 import pathlib
+from collections import Counter
 
 
 def get_input():
@@ -10,8 +11,44 @@ def get_input():
     return [line.strip() for line in input_raw]
 
 
+def get_most_common_bit(input_data, index):
+    index_data = [bits[index] for bits in input_data]
+
+    index_string = "".join(index_data)
+
+    bits_counter = Counter(index_string)
+
+    return bits_counter.most_common(1)[0][0]
+
+
+def get_least_common_bit(input_data, index):
+    index_data = [bits[index] for bits in input_data]
+
+    index_string = "".join(index_data)
+
+    bits_counter = Counter(index_string)
+
+    return bits_counter.most_common()[-1][0]
+
+
 def get_answer_part_1(input_data):
-    pass
+    num_bits = len(input_data[0])
+
+    gamma_rate_binary = ""
+
+    for i in range(num_bits):
+        gamma_rate_binary += get_most_common_bit(input_data, i)
+
+    gamma_rate = int(gamma_rate_binary, 2)
+
+    epsilon_rate_binary = ""
+
+    for i in range(num_bits):
+        epsilon_rate_binary += get_least_common_bit(input_data, i)
+
+    epsilon_rate = int(epsilon_rate_binary, 2)
+
+    return gamma_rate * epsilon_rate
 
 
 def get_answer_part_2(input_data):
